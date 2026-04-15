381c091c80e886c14d23ae67b8c0e3b0e7642fc4
Vitali Nikolaev <Vitalii.Nikolaev@constructor.tech>
2026-04-15 10:23:42 -0300

    Set base path for v2 repo

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/presence-prototype/',
+  base: '/presence-prototype-v2/',
   plugins: [
     tailwindcss(),
     react()
